8624b5b00332d9610f30da694db980176c9a8665
jamatija <matijatijanic@gmx.com>
2025-08-19 21:00:07 +0200

    redirect / to tasks index view

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-Route::get('/', function () {
-    return view('layout');
-});
+Route::get('/', [TaskController::class, 'index']);
 
 Route::resource('/tasks', TaskController::class);
 Route::put('/tasks/{task}/status', [TaskController::class, 'changeTaskStatus'])->name('tasks.changeStatus');
